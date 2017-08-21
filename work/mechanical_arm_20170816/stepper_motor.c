bd64df16c0e8109168e61ebed34294338383275d
chengyake <chengyake2008@163.com>
2017-08-21 13:53:29 +0800

    modify m-arm mcu

--- work/mechanical_arm_20170816/stepper_motor.c
+++ work/mechanical_arm_20170816/stepper_motor.c
@@ -8,16 +8,43 @@
 //理论精度 0.0005mm
 
 /*
-    *c
-    0:no cmd
-    1:begin to recev
-    5:recv all, need check
-    6:check over, run
+FANBC
+F：固定头0xFD
+A：A个方波/10ms
+N：A到B的自动计算填充N个数（10ms）
+B：A个方波/10ms
+C：异或校验
+
+clk_all = A+B+N;
+
+
+
+recv:
+    0 - 0xFA(250), clk num
+    0xFF:   dir 0
+    0xFE:   dir 1
+
+    cmd_c var:
+        0:no cmd
+        1:begin to recev
+        5:recv all, need check
+        6:check over, run
+
+return;
+    0xFB:   recv and crc ok
+    0xFC:   CRC Error
+    0xFD:   exec over;
+
 */
 
+#define RECV_OK     (0xFB)
+#define CRC_ERROR   (0xFC)
+#define EXEC_DONE   (0xFD)
+#define INTERVAL    (10000)
 
-sbit clk=P2^0;
-sbit dir=P2^1;
+
+sbit dir=P2^0;
+sbit clk=P2^1;
 
 unsigned char recv, us, step;
 unsigned char clk_num, clk_all;
@@ -41,6 +68,8 @@ void UARTInit(void) {
     T2CON=0x34; //T/C2 clk generator
     RCAP2L=0xD9; //9600 L 8bit
     RCAP2H=0xFF; //9600 H 8bit
+    ES = 1;
+    EA = 1;
 }
 
 void UARTSendByte(unsigned char byte) {
@@ -49,55 +78,49 @@ void UARTSendByte(unsigned char byte) {
     TI=0;
 }
 
-void recv_cmd() {
-    
-    if(RI) {
-        recv = SBUF;
-        RI = 0;
-        if((recv == 0xFF || recv == 0xFE) && c == 0) {
-            cmd[0] = recv;
-            dir = cmd[0]:0;
-            c = 1;
-        } else if (c > 0 && c < 5) {
-            cmd[c++] = recv;
-        }
-    }
+void StpperInit(void) {
+
+
+    P2=0xFF;    //init all high
+
 
 }
 
 
 void main(void) {
-
-    P2=0xFF;    //init all high
+    
+    StepperInit();
     UARTInit(); 
-    while(1) {    
+    while(1) {
 
-        us = 0; //default delay 10 ms
+        us = INTERVAL;
 
-        if(c == 5) {
+        if(cmd_c == 5) {
             if(cmd[1]^cmd[2]^cmd[3] == cmd[4]) {
-                c = 6;
+                cmd_c = 6;
                 step = 0;
                 clk_all = 0;
                 clk_num = 0;
+                UARTSendByte(RECV_OK);
             } else {
-                c = 0;
-                UARTSendByte(0xFF);
+                cmd_c = 0;
+                UARTSendByte(CRC_ERROR);
             }
         }
-        if(c == 6) {
-            clk_all = ((cmd[3]-cmd[1])/cmd[2]*step + cmd[1]);
+        if(cmd_c == 6) {
+            clk_all = (cmd[1] + (cmd[3]-cmd[1])/cmd[2]*step);
             if(clk_all == 0) {
-                us = 10000;
+                us = INTERVAL;
             } else {
-                us = 10000/clk_all;
+                us = INTERVAL/clk_all;
 
                 clk=~clk;
                 clk_num++;
                 if(clk_num >= clk_all) {
                     step++;
+                    clk_num = 0;
                     if(step >= cmd[2]+2) {
-                        c = 0;
+                        cmd_c = 0;
                     }
                 }
 
@@ -107,6 +130,25 @@ void main(void) {
 
         delay_us(us);
     }
+
+}
+
+
+void UartIRQ(void) interrupt    4 
+{
+
+    if(RI) {
+        recv = SBUF;
+        RI = 0;
+        if((recv == 0xFF || recv == 0xFE) && cmd_c == 0) {
+            cmd[0] = recv;
+            dir = cmd[0]:0;
+            cmd_c = 1;
+        } else if (cmd_c > 0 && cmd_c < 5) {
+            cmd[cmd_c++] = recv;
+        }
+    }
+
 
 }
 
