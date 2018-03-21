bda35ca0564b2e3dab3c99ca25de06851ea2edc3
chengyake <chengyake2008@163.com>
2018-03-21 17:44:04 +0800

    add work: somatic game for android

--- work/201803-20_android_somatic_games/main.py
+++ work/201803-20_android_somatic_games/main.py
@@ -1,14 +1,12 @@
 import cv2
+import os
 import numpy as np
-import copy
-import math
 
-import pyscreenshot as ImageGrab
-import time
-from pymouse import PyMouse
-import math
-from PIL import ImageFilter
-import random
+#import pyscreenshot as ImageGrab
+#import time
+#from pymouse import PyMouse
+#from PIL import ImageFilter
+#import random
 
 
 '''
@@ -21,8 +19,8 @@ import random
 '''
 
 # color b g r
-r_lower = np.array([0, 0, 135], dtype=np.uint8)
-r_upper = np.array([130, 130, 255], dtype=np.uint8)
+r_lower = np.array([100, 75, 178], dtype=np.uint8)
+r_upper = np.array([130, 104, 200], dtype=np.uint8)
 
 b_lower = np.array([120, 0, 0], dtype=np.uint8)
 b_upper = np.array([255, 120, 120], dtype=np.uint8)
@@ -40,19 +38,77 @@ def removeBG(frame):
     res = cv2.bitwise_and(frame, frame, mask=fgmask)
     return res
 
+
 def calc_center(gray):
-    num=0
+    x = 0
+    y = 0
+    rnpxy= np.sum(gray)
+    
+    if rnpxy < 10*255:
+        return 0, 0
+
+    rnpx = np.sum(gray, axis=0)
+    rnpy = np.sum(gray, axis=1)
+    
+    linesum=0
+    idx=0;
+    for i in rnpx:
+        if linesum >= rnpxy/2:
+            x = idx
+            break
+        linesum+=i
+        idx+=1
+
+
+    linesum=0
+    idx=0;
+    for i in rnpy:
+        if linesum >= rnpxy/2:
+            y = idx
+            break
+        linesum+=i
+        idx+=1
+
+    return x, y
+
+
+
+rl=[(0,0),(0,0),(0,0)]
+bl=[(0,0),(0,0),(0,0)]
+def filter_r(x,y):
+    global rl
+    rl.append((x,y))
+    rl.pop(0)
+    nznum = 3-rl.count((0,0))
+
+    if nznum == 0:
+        return 0, 0
+    
     xsum=0
     ysum=0
-    for x in range(gray.shape[0]):
-        for y in range(gray.shape[1]):
-            if gray[x][y] != 0:
-                xsum+=x
-                ysum+=y
-                num+=1
-    if num == 0:
+    for a,b in rl:
+        xsum+=a
+        ysum+=b
+    
+    return xsum/nznum, ysum/nznum
+
+
+def filter_b(x,y):
+    global bl
+    bl.append((x,y))
+    bl.pop(0)
+    nznum = 3-bl.count((0,0))
+
+    if nznum == 0:
         return 0, 0
-    return xsum/num, ysum/num
+    
+    xsum=0
+    ysum=0
+    for a,b in bl:
+        xsum+=a
+        ysum+=b
+    
+    return xsum/nznum, ysum/nznum
 
 
 # Camera
@@ -60,23 +116,20 @@ camera = cv2.VideoCapture(0)
 #camera.set(10,200)
 
 #delay
-for i in range(100):
+for i in range(60):
     ret, frame = camera.read()
+    #frame = cv2.GaussianBlur(frame, (11,11), 0)
     cv2.imshow('Camera-View',frame)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
-
 bgModel = cv2.BackgroundSubtractorMOG2(0, 50)
 while camera.isOpened():
     ret, frame = camera.read()
     #frame = cv2.bilateralFilter(frame, 5, 50, 100)  # smoothing filter
-    
-    frame = cv2.GaussianBlur(frame, (41,41), 0)
+    #frame = cv2.GaussianBlur(frame, (11,11), 0)
     frame = removeBG(frame)
     
-
-
     mask = cv2.inRange(frame, r_lower, r_upper)  
     r_frame = cv2.bitwise_and(frame, frame, mask = mask)  
     r_gray = cv2.cvtColor(r_frame, cv2.COLOR_BGR2GRAY)
@@ -85,14 +138,58 @@ while camera.isOpened():
     b_frame = cv2.bitwise_and(frame, frame, mask = mask)  
     b_gray = cv2.cvtColor(b_frame, cv2.COLOR_BGR2GRAY)
     
-    rx,ry = calc_center(r_gray)
-    cv2.line(r_gray, (rx, 0), (rx, 470), 255) #5
-    cv2.line(r_gray, (0, ry), (470, ry), 255) #5
+    rx, ry = calc_center(r_gray)
+    bx, by = calc_center(b_gray)
+    rx, ry = filter_r(rx, ry)
+    bx, by = filter_b(bx, by)
+
+    cv2.line(r_gray, (rx, 0), (rx, 470), 255) 
+    cv2.line(r_gray, (0, ry), (470, ry), 255) 
+    cv2.line(b_gray, (bx, 0), (bx, 470), 255) 
+    cv2.line(b_gray, (0, by), (470, by), 255) 
 
     cv2.imshow('Camera-View', frame)
     cv2.imshow('r-View', r_gray)
     cv2.imshow('b-View', b_gray)
-    cv2.waitKey(10)
+    cv2.waitKey(1)
+    
+
+
+
+def onPress(x, y):
+    return
+
+def onMove(x, y):
+    return
+
+def onRelease(x, y):
+    return
+
+switch=0
+def ontouch(x, y):
+    global switch
+    if (x,y) != (0, 0):
+        if switch==0:
+            onPress(x,y)
+            switch=1
+        else:
+            onMove(x,y)
+    else:
+        if switch!=0:
+            onRelease(x, y)
+            switch=0
+        else:
+            pass
+
+    return
+
+
+
+
+
+
+
+
 
 
 
@@ -100,14 +197,14 @@ while camera.isOpened():
 
 
 '''
-adb shell sendevent /dev/input/event0 3 0 110       //x坐标
-adb shell sendevent /dev/input/event0 3 1 70         //y坐标
-adb shell sendevent /dev/input/event0 1 330 1       //按下状态，准确的说是有压力值
-adb shell sendevent /dev/input/event0 0 0 0           //必要的一行数据
+adb shell sendevent /dev/input/event0 3 0 110       //x
+adb shell sendevent /dev/input/event0 3 1 70         //y
+adb shell sendevent /dev/input/event0 1 330 1       //
+adb shell sendevent /dev/input/event0 0 0 0           
 
 adb shell sendevent /dev/input/event0 3 0 101 //step to point (101,200)
 adb shell sendevent /dev/input/event0 0 0 0
 
-adb shell sendevent /dev/input/event0 1 330 0       //抬起状态，无压力值
-adb shell sendevent /dev/input/event0 0 0 0           //必要的一行，相当于终止一段完整数据的标致
+adb shell sendevent /dev/input/event0 1 330 0       //
+adb shell sendevent /dev/input/event0 0 0 0           //
 '''
