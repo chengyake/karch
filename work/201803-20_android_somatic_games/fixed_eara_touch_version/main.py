37125ffabb1cbccb0bf36ed0f08afcfc9896c60f
chengyake <chengyake2008@163.com>
2018-04-11 17:26:15 +0800

    modify bug for axis

--- work/201803-20_android_somatic_games/fixed_eara_touch_version/main.py
+++ work/201803-20_android_somatic_games/fixed_eara_touch_version/main.py
@@ -2,7 +2,8 @@ import cv2
 import os
 import time
 import copy
-import socket       
+import socket
+import math
 import numpy as np
 
 #import pyscreenshot as ImageGrab
@@ -61,20 +62,20 @@ class Minitouch:
 
     def down(self,i, (x,y)):
         self.s.send("d %d %d %d 50\n" %(i, x, y))
-        print("d %d %d %d 50\n" %(i, x, y))
+        #print("d %d %d %d 50\n" %(i, x, y))
         self.s.send("c\n")
         return
     
     def move(self,i, (x,y)):
         self.s.send("m %d %d %d 50\n" %(i, x, y))
-        print("m %d %d %d 50\n" %(i, x, y))
+        #print("m %d %d %d 50\n" %(i, x, y))
         self.s.send("c\n")
         return
     
     def up(self,i):
         self.s.send("u %d\n" % i)
-        print("u %d\n" % i)
-        self.s.send("c\n")
+        #print("u %d\n" % i)
+        #self.s.send("c\n")
         return
     
 
@@ -120,65 +121,81 @@ class Button:
                 touch.up(self.touch_index)
                 self.long_press=False
 
-
-
-class Wheel:
-    
-    trigger=False
-    long_press=False
-    index=0
+class Wheel: #bar
     def __init__(self):
-
-
-        dis_xy=[(138,155), (145,130), (157,108), (173,88), (193,72), (215,60), (240,53), (266,51), (292,51)]
-        act_xy=[(147,599), (121,593), (97, 577), (85, 556),(82, 534),(86, 511),(98, 491),(112,479),(129,471),
-        (147,469),(163,469),(185,481),(197,492),(209,513),(212,534),(210,554),(196,579),(177,594),(147,599)]
-        self.button_var_list=[]
-        self.button_trg_list=[False,False,False,False, False,False,False,False,False,
-                       False, False,False,False,False, False,False,False,False,False,]
-        for i in range(9):
-            self.button_var_list.append(Button(i, dis_xy[i], 15, act_xy[i]))
-        self.button_var_list.append(Button(9, (320,51), 15, act_xy[9]))
-        for i in range(10,19):
-            self.button_var_list.append(Button(i, (W-dis_xy[i-10][0], dis_xy[i-10][1]), 15, act_xy[i]))
-
-
-    def is_trigger(self, frame):
+        self.id = 0
+        self.position = ((140,60),(500,80))
+        self.mask = np.zeros((H, W, 3), dtype="uint8")
+        cv2.rectangle(self.mask,self.position[0],self.position[1],(255,255,255),-1)
         self.trigger=False
-        for i in range(19):
-            self.button_trg_list[i] = self.button_var_list[i].is_trigger(frame)
-            if self.button_trg_list[i]==True:
-                self.trigger=True
+        self.long_press=False
+        self.touch_index=0
+
+    def is_trigger(self, f):
+        frame=copy.deepcopy(f[50:70, 145:505])
+        frame[frame!=0]=1
+        s = np.sum(frame)
+        if s > 20*360*3/20:
+            self.trigger=True
+        if s < 20*360*3/22:
+            self.trigger=False
         return self.trigger
 
     def add_button_to_view(self, org_frame):
-        for i in range(len(self.button_var_list)):
-            org_frame = self.button_var_list[i].add_button_to_view(org_frame)
+        if self.trigger:
+            org_frame[self.mask==255]=255
+        else:
+            org_frame[self.mask==255]=0
+        return org_frame
+   
 
-    def action(self, touch, frame):
-
+    def action(self, touch, f):
         if self.trigger==True:
-            tmpx=0
-            tmpy=0
-            tmpi=0
-            for i in range(len(self.button_var_list)):
-                if self.button_var_list[i].is_trigger(frame):
-                    tmpi+=1
-                    tmpx+=self.button_var_list[i].act_point[0]
-                    tmpy+=self.button_var_list[i].act_point[1]
-            tmpx/=tmpi
-            tmpy/=tmpi
+            frame=f[50:70, 145:505]
+            asum=np.sum(frame)
+            yzsum=np.sum(frame, axis=0)
+            ysum = np.sum(yzsum, axis=1)
+            half_sum=0
+            y=0
+            for i in xrange(360):
+                half_sum+=ysum[i]
+                if half_sum>=asum/2:
+                    y=i
+                    break
+            act_x = 340-120*math.cos(float(y)*2.0/360.0*math.pi) #set act wheel point(x, y)
+            act_y = 370-120*math.sin(float(y)*2.0/360.0*math.pi)
+
             if self.long_press==False:
                 self.index=touch.get_index()
-                touch.down(self.index, (tmpx, tmpy))
+                touch.down(self.index, (act_x, act_y))
                 self.long_press=True
             else:
-                touch.move(self.index, (tmpx, tmpy))
+                touch.move(self.index, (act_x, act_y))
         
         if self.trigger==False and self.long_press==True:
             touch.up(self.index)
             touch.release_index(self.index)
             self.long_press=False
+            #patch for 4 button
+            index1=touch.get_index()
+            touch.down(index1, (1600-612, 304))
+            index2=touch.get_index()
+            touch.down(index2, (1600-750, 2144))
+            index3=touch.get_index()
+            touch.down(index3, (1600-942, 1804))
+            index4=touch.get_index()
+            touch.down(index4, (1600-1200, 1658))
+            touch.up(index1)
+            touch.release_index(index1)
+            touch.up(index2)
+            touch.release_index(index2)
+            touch.up(index3)
+            touch.release_index(index3)
+            touch.up(index4)
+            touch.release_index(index4)
+
+
+
 
 
 
@@ -191,7 +208,7 @@ class Camera:
 
     def frame_counter(self):
         self.frames+=1
-        print("frames: %d" % self.frames)
+        #print("frames: %d" % self.frames)
 
     def is_opened(self):
         return self.camera.isOpened()
@@ -230,15 +247,17 @@ def main():
     camera = Camera()
     wheel = Wheel()
     separate_button=[]
-    separate_button.append(Button(19, (210, 20), 18, (142,1173,))) #button_b   
-    separate_button.append(Button(20, (430, 20), 18, (192,1173,))) #button_g 
-    separate_button.append(Button(21, (130,220), 18, (592, 896,))) #button_s 
-    separate_button.append(Button(22, (144,280), 18, (467, 968,))) #button_m 
-    separate_button.append(Button(23, (160,340), 18, (394,1094,))) #button_l 
-    separate_button.append(Button(24, (320,220), 18, (200,2360,))) #button_a 
-    separate_button.append(Button(25, (502,220), 18, (604, 791,))) #button_1 
-    separate_button.append(Button(26, (496,280), 18, (604, 698,))) #button_2 
-    separate_button.append(Button(26, (480,340), 18, (604, 609,))) #button_3 
+    separate_button.append(Button(24, (320,220), 18, (1600-1368,2312))) #button_a 
+    separate_button.append(Button(19, (210, 20), 18, (1600- 330,2500))) #button_b   
+    separate_button.append(Button(20, (430, 20), 18, (1600- 460,2500))) #button_g 
+
+    separate_button.append(Button(21, (130,160), 18, (1600-1406,1836))) #button_s 
+    separate_button.append(Button(22, (144,220), 18, (1600-1098,2012))) #button_m 
+    separate_button.append(Button(23, (160,280), 18, (1600- 928,2314))) #button_l 
+
+    separate_button.append(Button(25, (502,160), 18, (1600-1430,1594))) #button_1 
+    separate_button.append(Button(26, (496,220), 18, (1600-1430,1366))) #button_2 
+    separate_button.append(Button(26, (480,280), 18, (1600-1420,1138))) #button_3 
 
     if not camera.is_opened():
         print("camera is not opened or unconnected")
@@ -246,7 +265,6 @@ def main():
     
     camera.stable_dur()
     camera.set_bg()
-    
     while camera.is_opened():
         
         camera.frame_counter()
@@ -279,7 +297,6 @@ def main():
 
 
 main()
-
 
 
 
