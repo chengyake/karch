e86112f064cd6ff7c349149da04c778fa46fefd9
chengyake <chengyake2008@163.com>
2018-06-19 12:56:50 +0800

    add snpe cmd

--- work/real_detect_2018_0606/snpe.sh
+++ work/real_detect_2018_0606/snpe.sh
@@ -5,3 +5,5 @@ python /usr/local/lib/python2.7/dist-packages/tensorflow/python/tools/optimize_f
 /opt/snpe-1.15.0/bin/x86_64-linux-clang/snpe-dlc-quantize --input_dlc model_yake.pb.dlc --input_list raw_list.txt --output_dlc model_yake_quantized.dlc
 }
 
+#/opt/snpe-1.15.0/bin/x86_64-linux-clang/snpe-net-run -container model_yake.pb.dlc --input_list raw_list.txt
+#/opt/snpe-1.15.0/bin/x86_64-linux-clang/snpe-dlc-info  -i model_yake.pb.dlc 
